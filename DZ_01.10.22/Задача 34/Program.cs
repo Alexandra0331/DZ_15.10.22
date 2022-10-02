63442897b7c1fe1bd19e8d52059d705c02dbd11d
Alexandra <Kaverzina0331@mail.ru>
2022-10-02 17:19:36 +0300

    Задача 36

--- DZ_01.10.22/Задача 34/Program.cs
+++ DZ_01.10.22/Задача 34/Program.cs
@@ -3,17 +3,6 @@
 //которая покажет количество чётных чисел в массиве.
 //[345, 897, 568, 234] -> 2
 
-
-//Console.WriteLine("размер массива: ");
-//int size = Convert.ToUInt32(Console.ReadLine());
-//int [] numbers = new int [size];
-//FillArrayRondomNumbers(numbers);
-//Console.WriteLine("массив: ");
-//PrintArray(numbers);
-//int count = 0;
-//for (int z = 0; z = numbers.Length; z++);
-//if (numbers [z] % 2 == 0);
-
 Console.Clear();
 Console.Write("Введите кол-во элементов массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
