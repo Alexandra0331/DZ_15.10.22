d842770dd61f87862cbaf6c1916d6808dcf5ea3b
Alexandra <Kaverzina0331@mail.ru>
2022-10-02 17:04:40 +0300

    Задача 41

--- DZ_02.10/Задача 41/Program.cs
+++ DZ_02.10/Задача 41/Program.cs
@@ -6,11 +6,11 @@
 // -1, -7, 567, 89, 223 -> 3
 
 Console.Write("Введите элементы(через пробел): ");
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 int count = 0;
-for (int i = 0; i < arr.Length; i++)
+for (int i = 0; i < array.Length; i++)
 {
-    if (arr[i] > 0)
+    if (array[i] > 0)
     {
         count++;
     }
